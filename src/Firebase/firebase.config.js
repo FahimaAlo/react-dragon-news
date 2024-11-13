96e1661851e1b37f531037453ae5c539595e2145
Fahima Alo <fahimaalo223@gmail.com>
2024-11-13 16:13:17 +0600

    update login & registration page

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA5AATvU7isH2n72EQ2KB0CHBCTu5MzRx4",
-  authDomain: "react-dragon-news-c25c2.firebaseapp.com",
-  projectId: "react-dragon-news-c25c2",
-  storageBucket: "react-dragon-news-c25c2.firebasestorage.app",
-  messagingSenderId: "282169803612",
-  appId: "1:282169803612:web:310acb5c4796661ced3799"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import userDefaultPic from '../../../assets/user.png'
+import { AuthContext } from '../../../Providers/AuthProvider';
 
 const Navbar = () => {
 
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleSignOut = () => {
+      logOut()
+      .then()
+      .catch()
+
+
+
+    }
     const navLinks = <>
         <li><NavLink to="/">Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/career">Career</NavLink></li>
+        <li><NavLink to="/addict">Addict</NavLink></li>
+        <li><NavLink to="/login">Login</NavLink></li>
+
     </>
     return (
         <div className="navbar bg-base-100">
@@ -48,9 +62,15 @@ const Navbar = () => {
             src={userDefaultPic} />
         </div>
       </div>
-            <Link>
-            <button className='btn'>Login</button>
-            </Link>
+        {
+          user ?
+          <button onClick={handleSignOut} className='btn'>Sign Out</button>
+          :
+          <Link to="/login">
+          <button className='btn'>Login</button>
+          </Link>
+        }
+           
           
         </div>
       </div>
